9d21ffec9447b20709859ce899344f5911f55999
theejkb <steven.copy@ynov.com>
2022-05-31 12:21:31 +0200

    add @

--- components/Profil.tsx
+++ components/Profil.tsx
@@ -1,19 +1,14 @@
 import { Avatar, Box, Text, WrapItem } from '@chakra-ui/react';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { accountState } from '../store/auth';
 
-
-
 export const Profil = () => {
-
   const [herotag, setHerotag] = useState('');
   const [nonce, setNonce] = useState('');
 
   async function getHerotag() {
     const res = await fetch(`https://api.elrond.com/accounts/${accountState.address}`);
     const data = await res.json();
-    console.log(data);
     const herotag = data?.username?.split('.')[0] ?? "";
     setHerotag(herotag);
     setNonce(data?.nonce);
@@ -29,7 +24,7 @@ export const Profil = () => {
         <Avatar size='xl' name='NaaQ' src='https://res.cloudinary.com/naaq/image/upload/v1653986673/t%C3%A9l%C3%A9chargement_aahft1.jpg' />
       </WrapItem>
       <Text mt='10px' textAlign='center'>{accountState.address}</Text>
-      <Text mt='10px' textAlign='center'>{herotag}</Text>
+      <Text mt='10px' textAlign='center'>@{herotag}</Text>
       <Text mt='10px' textAlign='center'>Nonce : {nonce}</Text>
      </Box>
      

2c7571542f95fd9f0e6a7e2dc88ecbd2222a2a34
Jeremy <jeremydouchamps@gmail.com>
2022-11-12 11:15:06 +0700

    Add sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://ghostverse.org',
+  siteUrl: process.env.SITE_URL || 'https://devnet-ghostverse.netlify.app/',
   generateRobotsTxt: true,
 };
